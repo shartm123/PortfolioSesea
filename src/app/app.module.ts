62b40c913b06239e0401b3a86b0ea92b8e74ef56
shartm123 <s.a.hartmann@bluewin.ch>
2022-09-13 17:14:25 +0200

    Basic general edits, for a first tryout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AvatarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
