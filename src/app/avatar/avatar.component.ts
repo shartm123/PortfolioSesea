8e8d5e650f585e7a582d1e271681207c5206c317
shartm123 <s.a.hartmann@bluewin.ch>
2022-10-06 10:05:03 +0200

    basic avatar setups

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -13,3 +13,19 @@ export class AvatarComponent implements OnInit {
   }
 
 }
+/**
+
+document.querySelector('body').addEventListener('mouseover', moveEyes);
+function moveEyes(){
+  let eye = document.querySelectorAll('.eye');
+  eye.forEach(function(eye){
+    let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+    let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+    let radian = Math.atan2(y,x);
+    let rot = (radian * (180 / Math.PI)* -1) + 270;
+    eye.style.transform = "rotate("+ rot +" deg)";
+    })
+}
+**/
+
