b9ca372852477a73c04c1340009609740c166aaa
shartm123 <s.a.hartmann@bluewin.ch>
2022-10-10 10:06:59 +0200

    working on responsive design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+function Menu(e){
+  let list = document.querySelector('ul');
+  e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
 }
